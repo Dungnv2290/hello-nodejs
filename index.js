39e7b67a2f7733eb2a011465005b6ec45b846f51
dungnguyen2290 <vandung2290@gmail.com>
2021-05-15 18:30:19 +0700

    Add Nodejs

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var app = express();
 app.get('/route', function(req, res){
 	res.send('Hello world')
 });
-app.listen(3000, function(){
+app.listen(5000, function(){
 	console.log('Example app listening on port 3000!');
 });
